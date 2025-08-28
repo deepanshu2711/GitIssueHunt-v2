3d81003844c5c116d232149b6cf78de13b24de49
Deepanshu <deepanshusaini2711@gmail.com>
2025-08-28 15:20:59 +0530

    chore(api): initialize express

---
+++ apps/api/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express + Turborepo 👋");
+});
+
+app.listen(5000, () => {
+  console.log(`🚀 API running at http://localhost:${500}`);
+});
