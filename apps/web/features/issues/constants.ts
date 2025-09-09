85c5952d1674da05d49ad3b797e751f35522b604
Deepanshu <deepanshusaini2711@gmail.com>
2025-09-09 11:42:50 +0530

    FEAT: Add issues filter by land and lable

---
+++ apps/web/features/issues/constants.ts
@@ -0,0 +1,11 @@
+import { Option } from "./types";
+
+export const options: Option[] = [
+  { value: "good first issue", label: "Good first issue" },
+  { value: "help wanted", label: "Help wanted" },
+  { value: "bug", label: "Bug" },
+  { value: "enhancement", label: "Enhancement" },
+  { value: "documentation", label: "Documentation" },
+  { value: "feature request", label: "Feature Request" },
+  { value: "gssoc25", label: "gssoc25" },
+];
